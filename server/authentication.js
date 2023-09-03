f5fbdbaa8a1b9f9728983e0c8557711abb8a5c3a
Pratik-Sarkate <pratiksarkate2242@gmail.com>
2023-09-04 00:40:51 +0530

    changed

--- server/authentication.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-const port = process.env.PORTA;
-
-authentication.listen(port, () =>
-  console.log(`Server running on port ${port}`)
-);
--- server/stripe.js
+++
@@ -1,7 +0,0 @@
-const configureStripe = require("stripe");
-
-const STRIPE_SECRET_KEY = process.env.NODE_ENV;
-
-const stripe = configureStripe(STRIPE_SECRET_KEY);
-
-module.exports = stripe;
